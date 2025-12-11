081edf43cf96ba8081734522fd4b85cd74b306d3
Nima Mokhtari <mokhtarinima56@gmail.com>
2025-12-11 18:20:07 +0330

    Kernel

---
+++ elf.c
@@ -0,0 +1,25 @@
+// kernel/elf.h
+#ifndef ELF_H
+#define ELF_H
+
+int load_elf(const char* path);
+
+#endif
+
+// kernel/elf.c - Basic ELF loader for user space
+#include "elf.h"
+#include "screen.h"
+#include "task.h"
+#include "memory.h"
+
+int load_elf(const char* path) {
+    // TODO: Read ELF from RAM disk (for now fake)
+    print("Loading ELF: ", 0x0A);
+    print(path, 0x0A);
+    print("\n", 0x0A);
+
+    // Fake entry point
+    uint64_t entry = 0x400000;
+    create_task((void(*)())entry, 3);  // New user task
+    return 0;
+}
